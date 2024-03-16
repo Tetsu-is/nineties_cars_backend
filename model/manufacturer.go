7f7d38d334a8a1728f4ae24fc6a70cc4085e3d54
Tetsu-is <hap30305@kwansei.ac.jp>
2024-03-17 01:16:58 +0900

    delete files

--- model/manufacturer.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type Manufacturer struct {
-	ID   uint   `json:"id"`
-	Name string `json:"name"`
-}
--- model/vehicle.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type Vehicle struct {
-	ID   uint   `json:"id"`
-	Name string `json:"name"`
-}
